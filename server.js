ad8fa1f4cdb2f2e666be169c0bb1dd7a73278a91
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-03-05 15:45:12 -0500

    set up connection to db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,5 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
+    useNewUrlParser: true,
+    UseUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 connected to server on port ${PORT}`));
