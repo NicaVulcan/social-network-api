2c4c1414cf3257d20e303780e0d78158b8eb09a3
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-03-05 16:35:22 -0500

    created user model and virtuals

--- models/User.js
+++ models/User.js
@@ -1,7 +1,48 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /.+@.+\..+/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
 
+// get total count of thoughts on retrieval
+ThoughtSchema.virtual('thoughtCount').get(function () {
+    return this.thoughts.length;
+});
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
 });
 
 const User = model('User', UserSchema);
