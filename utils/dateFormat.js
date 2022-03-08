1d401c4ba8d80b7adcdcb99d0a65c90e0604cf94
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-03-08 15:43:33 -0500

    completed date formatting util

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,5 +1,24 @@
 module.exports = (timestamp) => {
-    const dateObj = new Date(timestamp);
     
-    return dateObj;
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const dateObj = new Date(timestamp);
+    const month = months[dateObj.getMonth()];
+    const dayOfMonth = dateObj.getDate();
+    const year = dateObj.getFullYear();
+    let hour = dateObj.getHours();
+    const minute = dateObj.getMinutes();
+    let period = 'AM';
+
+    if (hour > 12) {
+      hour = hour - 12;
+      period = 'PM';
+    } 
+    if (hour === 0) {
+      hour = 12;
+      period = 'AM';
+    }
+
+    const formattedDate = `${month} ${dayOfMonth}, ${year} - ${hour}:${minute} ${period}`;
+    return formattedDate;
 } 
